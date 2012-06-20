13e5c2275342137c7775074473cc6fbd8240bdf8
sethvincent <sethvincent@gmail.com>
2012-06-20 15:57:36 -0700

    try other s3 ENV thing

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,7 +1,13 @@
 class Venue < ActiveRecord::Base
-  attr_accessible :address, :description, :name, :website, :wiki_url, :photo
-
+  attr_accessible :address, :description, :name, :website, :wiki_url, :photo, :events, :delete_photo
+  before_validation { self.photo.clear if self.photo_asset == '1' }
   has_many :events
   
-  has_attached_file :photo, :styles => { :large => "1200x1200", :medium => "600x600>", :small => "300x300", :thumb => "100x100>" }
+  has_attached_file :photo, :styles => { :large => "1200x1200", :medium => "600x600>", :small => "300x300", :thumb => "100x100>" },
+      :storage => :s3,
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :s3_credentials => {
+        :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+        :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      }
 end
--- config/initializers/paperclip.rb
+++
@@ -1,3 +0,0 @@
-Paperclip::Attachment.default_options[:storage] = :s3
-Paperclip::Attachment.default_options[:s3_credentials] = {:access_key_id => ENV['AWS_ACCESS_KEY_ID'], :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']}
-Paperclip::Attachment.default_options[:bucket] = ENV['S3_BUCKET_NAME']
