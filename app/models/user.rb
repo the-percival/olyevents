f68ba34dbafb1433d6028f61180ee18db32b6d87
sethvincent <sethvincent@gmail.com>
2012-06-20 19:19:05 -0700

    huh

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,5 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
-  
-  has_many :events
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :admin
 end
