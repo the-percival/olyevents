7fa1953b3ce76de7e0140b3daf3d62cfa8ddea7f
sethvincent <sethvincent@gmail.com>
2012-06-22 13:22:52 -0700

    make tags work

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ActiveRecord::Base
   acts_as_taggable
   acts_as_indexed :fields => [:name, :description, :venue, :tags]
-  attr_accessible :all_ages, :cost, :description, :end_date, :featured, :name, :start_date, :venue_id, :photo, :delete_photo
+  attr_accessible :all_ages, :cost, :description, :end_date, :featured, :name, :start_date, :venue_id, :photo, :delete_photo, :tag_list
   belongs_to :venue
   
   has_attached_file :photo, :styles => { :large => "1200x1200", :medium => "600x600>", :thumb => "100x100>" },
