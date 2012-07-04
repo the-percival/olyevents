906144899ca72cf7ae1fb1dbc5368c59b93433c4
sethvincent <sethvincent@gmail.com>
2012-07-04 12:24:14 -0700

    add default to in mailer

--- app/mailers/contact.rb
+++ app/mailers/contact.rb
@@ -1,6 +1,7 @@
 class Contact < ActionMailer::Base
+  default to: "heybuddy@olylab.com"
   default from: "heybuddy@olylab.com"
-  
+
   def new_message(message)
     @message = message
     mail(:subject => "[olyevents] #{message.subject} from #{message.name}")
